c6d0e6c498380c8e64066049dd68ebb9ad79c61f
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2025-05-06 19:25:54 +0200

    chore: add base electron app config

--- streaming-client/vite.main.config.ts
+++ streaming-client/vite.main.config.ts
@@ -1,4 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    // For Node.js native modules
+    conditions: ["node"], // this is the change
+    mainFields: ["module", "jsnext:main", "jsnext"],
+  },
+  plugins: [
+    {
+      name: "restart",
+      closeBundle() {
+        process.stdin.emit("data", "rs");
+      },
+    },
+  ],
+});
